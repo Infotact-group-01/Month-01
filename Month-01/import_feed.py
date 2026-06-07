26ed60418ba572685b91d1cb8877c4e02f535962
Harnisha031 <savaliyaharnisha@gmail.com>
2026-06-07 13:12:33 +0530

    Update import_feed.py

--- Month-01/import_feed.py
+++ Month-01/import_feed.py
@@ -1,7 +1,7 @@
 import csv
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://pensive_turing:27017")
+client = MongoClient(...)
 
 db = client["threat_intelligence"]
 collection = db["threats"]
